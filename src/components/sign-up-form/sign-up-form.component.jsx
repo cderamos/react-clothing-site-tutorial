0412ac3f71670a47852eb2ad5550ee65834011bb
Charmaine De Ramos <charm.deramos@gmail.com>
2022-12-03 18:50:35 -0500

    Updated sign-in page

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,9 @@
 import { errorPrefix } from '@firebase/util';
 import {useState} from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
-
+import FormInput from '../form-input/form-input.component';
+import './sign-up-form.styles.scss';
+import Button from '../button/button.component';
 
 const defaultFormFields = {
     displayName: '',
@@ -54,21 +56,57 @@ const handleSubmit = async (event) => {
     };
 
     return (
-        <div>
-            <h1> Sign up with your email</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span> Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
-                <label> Display Name </label>
-                <input  type="text" required onChange={handleChange} name="displayName" value={displayName}/>
+                <FormInput
+                    label="DisplayName"
+                    inputOptions= {{
+                        type:'text',
+                        required:true,
+                        onChange:handleChange,
+                        name:'displayName',
+                        value:displayName,
+                    }}
+                />
+                    
 
-                <label> Email </label>
-                <input type="email" required onChange={handleChange} name="email" value={email}/>
+                <FormInput
+                    label="Email"
+                    type='email'
+                    inputOptions= {{
+                        type:'email',
+                        required:true,
+                        onChange:handleChange,
+                        name:'email',
+                        value:email,
+                    }}
+                />
+                
+                <FormInput
+                    label="Password"
+                    inputOptions= {{
+                        type:'password',
+                        required:true,
+                        onChange:handleChange,
+                        name:'password',
+                        value:password,
+                    }}
+                />
 
-                <label> Password</label>
-                <input type="password" required onChange={handleChange} name="password" value={password}/>
+                <FormInput
+                    label="Confirm Password"
+                    inputOptions= {{
+                        type:'password',
+                        required:true,
+                        onChange:handleChange,
+                        name:'confirmPassword',
+                        value:confirmPassword,
+                    }}
+                />
 
-                <label> Confirm Password </label>
-                <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
-                <button type="submit"> Sign Up </button>
+                <Button type="submit"> Sign Up </Button>
             </form>
         </div>
     );
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,7 +3,6 @@ import { getRedirectResult } from 'firebase/auth';
 import { 
         auth,
         signInWithGooglePopup, 
-        signInWithGoogleRedirect, 
         createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
@@ -27,9 +26,6 @@ const logGoogleUser =  async () => {
             <h1> Sign In Page </h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with Google Redirect
             </button>
             <SignUpForm/>
         </div>
